466ab854e644ba0a7593fe4dc878c11ac235f438
Haelbarde <haelbarde@gmail.com>
2022-10-22 20:10:02 +1030

    Added Debug class for more control over debug logging

---
+++ KKC Test 2/Debug.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KKC_Test_2
+{
+    internal class Debug
+    {
+        static bool Enabled = true;
+        static DateTime lastTime = DateTime.Now;
+        public Debug()
+        {
+
+        }
+        public static void Log(string message)
+        {
+            if (Enabled)
+            {
+                var current = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Green;
+                string timeDif = (DateTime.Now - lastTime).TotalMilliseconds.ToString();
+                lastTime = DateTime.Now;
+                Console.WriteLine($"{DateTime.Now.ToString("hh:mm:ss:fff")} ({timeDif}ms): {message}");
+                Console.ForegroundColor = current;
+            }
+            
+        }
+    }
+}
